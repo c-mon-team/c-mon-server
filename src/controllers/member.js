006a3ed5d9b697a4d6080bf063370745d9f6d979
namjiyun <namjiyun3275@gmail.com>
2022-11-20 14:30:07 +0900

    [FEAT] choice 저장

--- src/controllers/member.js
+++ src/controllers/member.js
@@ -6,9 +6,7 @@ const memberService = require('../services/memberService');
 
 const createMemberChoice = async (req, res) => {
   const { groupId, userName, choice } = req.body;
-  console.log(groupId);
-  console.log(userName);
-  console.log(choice[0]);
+
   if (!groupId || !userName || !choice) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
   let client;
   try {
@@ -18,6 +16,9 @@ const createMemberChoice = async (req, res) => {
     res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SUCCESS, data));
   } catch (error) {
     console.log(error);
+    if (error == 404) {
+      return res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NOTEXIST));
+    }
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
--- src/services/memberService.js
+++ src/services/memberService.js
@@ -10,12 +10,31 @@ const createMemberChoice = async (client, groupId, userName, choice) => {
     `,
     [userName, groupId],
   );
-  // const member = createMember[0].id
-  // choice.map(x =>
-  //     member
-  // );
-
-  return convertSnakeToCamel.keysToCamel(member[0]);
+  const member = createMember[0].id;
+  for (let c of choice) {
+    const { rows: subcategory } = await client.query(
+      `
+        SELECT *
+        FROM subcategory
+        WHERE name = $1
+        `,
+      [c],
+    );
+    if (!subcategory[0]) {
+      throw 404;
+    }
+    console.log(subcategory);
+    const { rows: choice } = await client.query(
+      `
+        INSERT INTO choice
+        (member_id, subcategory_id)
+        VALUES ($1, $2)
+        RETURNING *
+        `,
+      [member, subcategory[0].id],
+    );
+  }
+  return convertSnakeToCamel.keysToCamel(createMember);
 };
 
 module.exports = { createMemberChoice };
